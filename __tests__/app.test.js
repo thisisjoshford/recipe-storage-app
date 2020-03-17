8db3d918c374d2a866ef3d6e407c8f4663ac82f3
Josh Ford <thisisjoshford@gmail.com>
2020-03-17 15:10:52 -0700

    deleted route tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -122,6 +122,28 @@ describe('app routes', () => {
       });
   });
 
+  it('deletes a recipe by id', async() => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+
+    return request(app)
+      .delete(`/api/v1/recipes/${recipe._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: res.body.name,
+          directions: res.body.directions,
+          __v: 0
+        });
+      });
+  });
 
 
 });
